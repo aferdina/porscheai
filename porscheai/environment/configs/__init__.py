6798d60d4867f1e6a84d0718e401b52bb1bf9459
Andre Ferdinand <aferdina@mail.uni-mannheim.de>
2023-08-20 08:48:07 +0200

    update inits in configs

--- porscheai/environment/configs/__init__.py
+++ porscheai/environment/configs/__init__.py
@@ -1,13 +1,7 @@
-from .actions_space import *
 from .car_physics import *
 from .target_trajectory import *
-from .observation_space_examples import *
-from .general_configs import *
 from .abstract_classes import *
 
-__all__ = actions_space.__all__
 __all__ += car_physics.__all__
 __all__ += target_trajectory.__all__
-__all__ += observation_space_examples.__all__
-__all__ += general_configs.__all__
 __all__ += abstract_classes.__all__
