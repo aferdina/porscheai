262c695a18a71d0241212fe4393538e92f6063b8
Andre Ferdinand <aferdina@mail.uni-mannheim.de>
2023-08-21 10:55:21 +0200

    bugfix init file

--- porscheai/environment/configs/__init__.py
+++ porscheai/environment/configs/__init__.py
@@ -2,6 +2,6 @@ from .car_physics import *
 from .target_trajectory import *
 from .abstract_classes import *
 
-__all__ += car_physics.__all__
+__all__ = car_physics.__all__
 __all__ += target_trajectory.__all__
 __all__ += abstract_classes.__all__
